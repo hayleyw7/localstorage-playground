db6ecb9e9a43b5bc99d86c54518f7433391678e2
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-15 12:35:09 -0600

    Try with Andrew's code

--- scripts.js
+++ scripts.js
@@ -1,17 +1,24 @@
-/* Use this area for pseudo - coding:
-
-
-
-*/
-
 // Query selectors
 
-
+var names = document.querySelector('#name')
+var email = document.querySelector('#email')
+var registerBtn = document.querySelector('#register-btn')
+var displayBtn = document.querySelector('#display-btn')
 
 // Event listeners
 
-
-
+registerBtn.addEventListener('click', addContact)
 
 // Functions
 
+class EmailInput {
+  constructor(names, email) {
+    this.name = names.value
+    this.email = email.value
+  }
+}
+function addContact() {
+  var newEmail = new EmailInput(names, email)
+  var string = JSON.stringify(newEmail)
+  var parsed = localStorage.setItem('newEmail', string)
+}
